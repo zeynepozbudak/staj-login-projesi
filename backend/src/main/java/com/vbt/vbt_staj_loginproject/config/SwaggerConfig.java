6505bb9a7740f90463d667e6bebc203445da4e81
1Mery <meryem.ozevren442001@gmail.com>
2026-07-23 09:49:37 +0300

    add Swagger/OpenAPI configuration

---
+++ backend/src/main/java/com/vbt/vbt_staj_loginproject/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.vbt.vbt_staj_loginproject.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("VBT Staj - Login API")
+                        .description("JWT tabanli kimlik dogrulama API'si. " +
+                                "Access token + Refresh token (HttpOnly Cookie) yapisi, " +
+                                "Redis ile token yonetimi, Argon2 ile sifre hashleme.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Meryem")
+                                .email("elagunes484@gmail.com")));
+    }
+}
